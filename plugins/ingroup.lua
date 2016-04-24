7df9dfa1ddd55ce5c60e57a447dff6b39503f53b
ereizertmbot <mohamadsabori55@gmail.com>
2016-04-24 13:41:09 +0430

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -1315,7 +1315,7 @@ end
 			return unlock_group_sticker(msg, data, target)
 		end
 		if matches[2] == 'contacts' then
-			savelog(msg.to.id, name_log.." ["..msg.from.id.."] unlocked contact posting")
+			savelog(msg.to.id, name_log.." ["..msg.from.id.."] ارسال contact آزاد شد")
 			return unlock_group_contacts(msg, data, target)
 		end
 	end
@@ -1325,153 +1325,153 @@ end
 
   if matches[1] == 'mute' and is_owner(msg) then
 			local chat_id = msg.to.id
-			if matches[2] == 'audio' then
+			if matches[2] == 'صدا' then
 			local msg_type = 'Audio'
 				if not is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: mute "..msg_type)
 					mute(chat_id, msg_type)
-					return "Group "..matches[2].." has been muted"
+					return "Group "..matches[2].." موت شد"
 				else
-					return "Group mute "..matches[2].." is already on"
+					return "Group mute "..matches[2].." قبلا فعال بود"
 				end
 			end
-			if matches[2] == 'photo' then
+			if matches[2] == 'عکس' then
 			local msg_type = 'Photo'
 				if not is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: mute "..msg_type)
 					mute(chat_id, msg_type)
-					return "Group "..matches[2].." has been muted"
+					return "Group "..matches[2].." موت شد"
 				else
-					return "Group mute "..matches[2].." is already on"
+					return "Group mute "..matches[2].." قبلا فعال بود"
 				end
 			end
-			if matches[2] == 'video' then
+			if matches[2] == 'فیلم' then
 			local msg_type = 'Video'
 				if not is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: mute "..msg_type)
 					mute(chat_id, msg_type)
-					return "Group "..matches[2].." has been muted"
+					return "Group "..matches[2].." موت شد"
 				else
-					return "Group mute "..matches[2].." is already on"
+					return "Group mute "..matches[2].." قبلا فعال بود"
 				end
 			end
-			if matches[2] == 'gifs' then
+			if matches[2] == 'گیف' then
 			local msg_type = 'Gifs'
 				if not is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: mute "..msg_type)
 					mute(chat_id, msg_type)
-					return msg_type.." have been muted"
+					return msg_type.." موت شد"
 				else
-					return "Group mute "..msg_type.." is already on"
+					return "Group mute "..msg_type.." قبلا فعال بود"
 				end
 			end
-			if matches[2] == 'documents' then
+			if matches[2] == 'فایل' then
 			local msg_type = 'Documents'
 				if not is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: mute "..msg_type)
 					mute(chat_id, msg_type)
-					return msg_type.." have been muted"
+					return msg_type.." موت شد"
 				else
-					return "Group mute "..msg_type.." is already on"
+					return "Group mute "..msg_type.." قبلا فعال بود"
 				end
 			end
-			if matches[2] == 'text' then
+			if matches[2] == 'متن' then
 			local msg_type = 'Text'
 				if not is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: mute "..msg_type)
 					mute(chat_id, msg_type)
-					return "Group text has been muted"
+					return "چت کردن موت شد"
 				else
-					return "Group mute text is already on"
+					return "چت کردن قبلا فعال بود"
 				end
 			end
-			if matches[2] == 'all' then
+			if matches[2] == 'همه' then
 			local msg_type = 'All'
 				if not is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: mute "..msg_type)
 					mute(chat_id, msg_type)
-					return "Mute "..msg_type.."  has been enabled"
+					return "Mute "..msg_type.."  فعال شد"
 				else
-					return "Mute "..msg_type.." is already on"
+					return "Mute "..msg_type.." قبلا فعال بود"
 				end
 			end
 		end
-		if matches[1] == 'unmute' and is_owner(msg) then
+		if matches[1] == 'آزاد' and is_owner(msg) then
 			local chat_id = msg.to.id
-			if matches[2] == 'audio' then
+			if matches[2] == 'صدا' then
 			local msg_type = 'Audio'
 				if is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: unmute "..msg_type)
 					unmute(chat_id, msg_type)
-					return "Group "..msg_type.." has been unmuted"
+					return "Group "..msg_type.." غیر فعال شد"
 				else
-					return "Group mute "..msg_type.." is already off"
+					return "Group mute "..msg_type.." قبلا غیر فعال شد"
 				end
 			end
-			if matches[2] == 'photo' then
+			if matches[2] == 'عکس' then
 			local msg_type = 'Photo'
 				if is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: unmute "..msg_type)
 					unmute(chat_id, msg_type)
-					return "Group "..msg_type.." has been unmuted"
+					return "Group "..msg_type.." غیر فعال شد"
 				else
-					return "Group mute "..msg_type.." is already off"
+					return "Group mute "..msg_type.." قبلا غیر فعال بود"
 				end
 			end
-			if matches[2] == 'Video' then
+			if matches[2] == 'فیلم' then
 			local msg_type = 'Video'
 				if is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: unmute "..msg_type)
 					unmute(chat_id, msg_type)
-					return "Group "..msg_type.." has been unmuted"
+					return "Group "..msg_type.." آزاد شد"
 				else
-					return "Group mute "..msg_type.." is already off"
+					return "Group mute "..msg_type.." قبلا آزاد بود"
 				end
 			end
-			if matches[2] == 'gifs' then
+			if matches[2] == 'گیف' then
 			local msg_type = 'Gifs'
 				if is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: unmute "..msg_type)
 					unmute(chat_id, msg_type)
-					return msg_type.." have been unmuted"
+					return msg_type.." آزاد شد"
 				else
-					return "Mute "..msg_type.." is already off"
+					return "Mute "..msg_type.." قبلا آزاد بود"
 				end
 			end
-			if matches[2] == 'documents' then
+			if matches[2] == 'فایل' then
 			local msg_type = 'Documents'
 				if is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: unmute "..msg_type)
 					unmute(chat_id, msg_type)
-					return msg_type.." have been unmuted"
+					return msg_type.." آزاد شد"
 				else
-					return "Mute "..msg_type.." is already off"
+					return "Mute "..msg_type.." قبلا آزاد بود"
 				end
 			end
-			if matches[2] == 'text' then
+			if matches[2] == 'متن' then
 			local msg_type = 'Text'
 				if is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: unmute message")
 					unmute(chat_id, msg_type)
-					return "Group text has been unmuted"
+					return "چت آزاد شد"
 				else
-					return "Group mute text is already off"
+					return "چت قبلا آزاد بود"
 				end
 			end
-			if matches[2] == 'all' then
+			if matches[2] == 'همه' then
 			local msg_type = 'All'
 				if is_muted(chat_id, msg_type..': yes') then
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: unmute "..msg_type)
 					unmute(chat_id, msg_type)
-					return "Mute "..msg_type.." has been disabled"
+					return "Mute "..msg_type.." آزاد شد"
 				else
-					return "Mute "..msg_type.." is already disabled"
+					return "Mute "..msg_type.." قبلا آزاد بود"
 				end
 			end
 		end
 
 	--Begin chat muteuser
-		if matches[1] == "muteuser" and is_momod(msg) then
+		if matches[1] == "سکوت" and is_momod(msg) then
 		local chat_id = msg.to.id
 		local hash = "mute_user"..chat_id
 		local user_id = ""
@@ -1479,16 +1479,16 @@ end
 				local receiver = get_receiver(msg)
 				local get_cmd = "mute_user"
 				get_message(msg.reply_id, mute_user_callback, {receiver = receiver, get_cmd = get_cmd})
-			elseif matches[1] == "muteuser" and string.match(matches[2], '^%d+$') then
+			elseif matches[1] == "سکوت" and string.match(matches[2], '^%d+$') then
 				local user_id = matches[2]
 				if is_muted_user(chat_id, user_id) then
 					mute_user(chat_id, user_id)
-					return "["..user_id.."] removed from the muted users list"
+					return "["..user_id.."] از لیست سکوت حذف شد"
 				else
 					unmute_user(chat_id, user_id)
-					return "["..user_id.."] added to the muted user list"
+					return "["..user_id.."] به لیست سکوت اضافه شد"
 				end
-			elseif matches[1] == "muteuser" and not string.match(matches[2], '^%d+$') then
+			elseif matches[1] == "سکوت" and not string.match(matches[2], '^%d+$') then
 				local receiver = get_receiver(msg)
 				local get_cmd = "mute_user"
 				local username = matches[2]
@@ -1498,7 +1498,7 @@ end
 		end
 
   --End Chat muteuser
-  	if matches[1] == "muteslist" and is_momod(msg) then
+  	if matches[1] == "لیست سکوت" and is_momod(msg) then
 		local chat_id = msg.to.id
 		if not has_mutes(chat_id) then
 			set_mutes(chat_id)
@@ -1507,39 +1507,39 @@ end
 		savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup muteslist")
 		return mutes_list(chat_id)
 	end
-	if matches[1] == "mutelist" and is_momod(msg) then
+	if matches[1] == "لیست سکوت" and is_momod(msg) then
 		local chat_id = msg.to.id
 		savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup mutelist")
 		return muted_user_list(chat_id)
 	end
 
-    if matches[1] == 'settings' and is_momod(msg) then
+    if matches[1] == 'تنظیمات' and is_momod(msg) then
       local target = msg.to.id
       savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group settings ")
       return show_group_settingsmod(msg, target)
     end
 
- if matches[1] == 'public' and is_momod(msg) then
+ if matches[1] == 'عمومی' and is_momod(msg) then
     local target = msg.to.id
     if matches[2] == 'yes' then
       savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: public")
       return set_public_membermod(msg, data, target)
     end
-    if matches[2] == 'no' then
+    if matches[2] == 'خیر' then
       savelog(msg.to.id, name_log.." ["..msg.from.id.."] set group to: not public")
       return unset_public_membermod(msg, data, target)
     end
   end
 
 if msg.to.type == 'chat' then
-    if matches[1] == 'newlink' and not is_realm(msg) then
+    if matches[1] == 'لینک جدید' and not is_realm(msg) then
       if not is_momod(msg) then
-        return "For moderators only!"
+        return "شما نمیتوانید"
       end
       local function callback (extra , success, result)
         local receiver = 'chat#'..msg.to.id
         if success == 0 then
-           return send_large_msg(receiver, '*Error: Invite link failed* \nReason: Not creator.')
+           return send_large_msg(receiver, '*Error: خطا.')
         end
         send_large_msg(receiver, "Created a new link")
         data[tostring(msg.to.id)]['settings']['set_link'] = result
@@ -1549,43 +1549,43 @@ if msg.to.type == 'chat' then
       savelog(msg.to.id, name_log.." ["..msg.from.id.."] revoked group link ")
       return export_chat_link(receiver, callback, true)
     end
-    if matches[1] == 'link' then
+    if matches[1] == 'لینک' then
       if not is_momod(msg) then
-        return "For moderators only!"
+        return "شما نمیتوانید"
       end
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
       if not group_link then
-        return "Create a link using /newlink first !"
+        return "اول با دستور لینک جدید لینک بسازید سپس اقدام کنید"
       end
        savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group link ["..group_link.."]")
-      return "Group link:\n"..group_link
+      return "لینک گروه:\n"..group_link
     end
-    if matches[1] == 'setowner' and matches[2] then
+    if matches[1] == 'صاحب' and matches[2] then
       if not is_owner(msg) then
-        return "For owner only!"
+        return "شما نمیتوانید"
       end
       data[tostring(msg.to.id)]['set_owner'] = matches[2]
       save_data(_config.moderation.data, data)
       savelog(msg.to.id, name_log.." ["..msg.from.id.."] set ["..matches[2].."] as owner")
-      local text = matches[2].." added as owner"
+      local text = matches[2].." صاحب گروه شد"
       return text
     end
-    if matches[1] == 'setowner' and not matches[2] then
+    if matches[1] == 'صاحب' and not matches[2] then
       if not is_owner(msg) then
-        return "only for the owner!"
+        return "شما نمیتوانید"
       end
       if type(msg.reply_id)~="nil" then
           msgr = get_message(msg.reply_id, setowner_by_reply, false)
       end
     end
 end
-    if matches[1] == 'owner' then
+    if matches[1] == 'دارنده' then
       local group_owner = data[tostring(msg.to.id)]['set_owner']
       if not group_owner then
-        return "no owner,ask admins in support groups to set owner for your group"
+        return "مدیری در این گروه وجود ندارد"
       end
       savelog(msg.to.id, name_log.." ["..msg.from.id.."] used /owner")
-      return "Group owner is ["..group_owner..']'
+      return "صاحب فعلی گروه: ["..group_owner..']'
     end
     if matches[1] == 'setgpowner' then
       local receiver = "chat#id"..matches[2]
@@ -1598,59 +1598,59 @@ end
       send_large_msg(receiver, text)
       return
     end
-    if matches[1] == 'setflood' then
+    if matches[1] == 'حساسیت' then
       if not is_momod(msg) then
-        return "For moderators only!"
+        return "شما نمیتوانید"
       end
       if tonumber(matches[2]) < 5 or tonumber(matches[2]) > 20 then
-        return "Wrong number,range is [5-20]"
+        return "فقط اعداد [5-20]"
       end
       local flood_max = matches[2]
       data[tostring(msg.to.id)]['settings']['flood_msg_max'] = flood_max
       save_data(_config.moderation.data, data)
       savelog(msg.to.id, name_log.." ["..msg.from.id.."] set flood to ["..matches[2].."]")
-      return 'Group flood has been set to '..matches[2]
+      return 'حساسیت پیام مکرر تعقیر یافت '..matches[2]
     end
 
 if msg.to.type == 'chat' then
-    if matches[1] == 'clean' then
+    if matches[1] == 'پاکسازی' then
       if not is_owner(msg) then
-        return "Only owner can clean"
+        return "شما نمیتوانید"
       end
-      if matches[2] == 'member' then
+      if matches[2] == 'اعضا' then
         if not is_owner(msg) then
-          return "Only admins can clean members"
+          return "شما نمیتوانید"
         end
         local receiver = get_receiver(msg)
         chat_info(receiver, cleanmember, {receiver=receiver})
       end
 	 end
-      if matches[2] == 'modlist' then
+      if matches[2] == 'مدیر' then
         if next(data[tostring(msg.to.id)]['moderators']) == nil then --fix way
-          return 'No moderator in this group.'
+          return 'مدیری در این گروه وجود ندارد'
         end
         local message = '\nList of moderators for ' .. string.gsub(msg.to.print_name, '_', ' ') .. ':\n'
         for k,v in pairs(data[tostring(msg.to.id)]['moderators']) do
           data[tostring(msg.to.id)]['moderators'][tostring(k)] = nil
           save_data(_config.moderation.data, data)
         end
-        savelog(msg.to.id, name_log.." ["..msg.from.id.."] cleaned modlist")
+        savelog(msg.to.id, name_log.." ["..msg.from.id.."] پاکسازی شد")
       end
-      if matches[2] == 'rules' then
+      if matches[2] == 'قانون' then
         local data_cat = 'rules'
         data[tostring(msg.to.id)][data_cat] = nil
         save_data(_config.moderation.data, data)
-        savelog(msg.to.id, name_log.." ["..msg.from.id.."] cleaned rules")
+        savelog(msg.to.id, name_log.." ["..msg.from.id.."] پاکسازی شد")
       end
-      if matches[2] == 'about' then
+      if matches[2] == 'موضوع' then
         local data_cat = 'description'
         data[tostring(msg.to.id)][data_cat] = nil
         save_data(_config.moderation.data, data)
-        savelog(msg.to.id, name_log.." ["..msg.from.id.."] cleaned about")
+        savelog(msg.to.id, name_log.." ["..msg.from.id.."] پاکسازی شد")
       end
     end
 if msg.to.type == 'chat' then
-    if matches[1] == 'kill' and matches[2] == 'chat' then
+    if matches[1] == 'حذف' and matches[2] == 'chat' then
       if not is_admin1(msg) then
           return nil
       end
@@ -1660,10 +1660,10 @@ if msg.to.type == 'chat' then
           print("Closing Group..."),
           chat_info(receiver, killchat, {receiver=receiver})
       else
-          return 'This is a realm'
+          return 'خطا'
       end
    end
-    if matches[1] == 'kill' and matches[2] == 'realm' then
+    if matches[1] == 'حذف' and matches[2] == 'realm' then
      if not is_admin1(msg) then
          return nil
      end
@@ -1673,17 +1673,17 @@ if msg.to.type == 'chat' then
         print("Closing Realm..."),
         chat_info(receiver, killrealm, {receiver=receiver})
      else
-        return 'This is a group'
+        return 'خطا'
      end
    end
-    if matches[1] == 'help' then
+    if matches[1] == 'راهنما' then
       if not is_momod(msg) or is_realm(msg) then
         return
       end
       savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used /help")
       return help()
     end
-    if matches[1] == 'res' then 
+    if matches[1] == 'ایدی' then 
       local cbres_extra = {
         chatid = msg.to.id
       }
@@ -1692,10 +1692,10 @@ if msg.to.type == 'chat' then
       resolve_username(username,  callbackres, cbres_extra)
 	  return
     end
-    if matches[1] == 'kickinactive' then
+    if matches[1] == 'حذف غیرفعال' then
       --send_large_msg('chat#id'..msg.to.id, 'I\'m in matches[1]')
 	    if not is_momod(msg) then
-	      return 'Only a moderator can kick inactive users'
+	      return 'شما نمیتوانید'
 	    end
 	    local num = 1
 	    if matches[2] then
@@ -1718,44 +1718,44 @@ end
 
 return {
   patterns = {
-  "^[#!/](add)$",
-  "^[#!/](add) (realm)$",
-  "^[#!/](rem)$",
-  "^[#!/](rem) (realm)$",
-  "^[#!/](rules)$",
-  "^[#!/](about)$",
-  "^[#!/](setname) (.*)$",
-  "^[#!/](setphoto)$",
-  "^[#!/](promote) (.*)$",
-  "^[#!/](promote)",
-  "^[#!/](help)$",
-  "^[#!/](clean) (.*)$",
-  "^[#!/](kill) (chat)$",
-  "^[#!/](kill) (realm)$",
-  "^[#!/](demote) (.*)$",
-  "^[#!/](demote)",
-  "^[#!/](set) ([^%s]+) (.*)$",
-  "^[#!/](lock) (.*)$",
-  "^[#!/](setowner) (%d+)$",
-  "^[#!/](setowner)",
-  "^[#!/](owner)$",
-  "^[#!/](res) (.*)$",
+  "^(اضافه)$",
+  "^(اضافه) (ریلم)$",
+  "^(ریمو)$",
+  "^(ریمو) (ریلم)$",
+  "^(قوانین)$",
+  "^(موضوع)$",
+  "^(تنظیم نام) (.*)$",
+  "^(تنظیم عکس)$",
+  "^(ترفیع) (.*)$",
+  "^(ترفیع)",
+  "^(راهنما)$",
+  "^(پاکسازی) (.*)$",
+  "^(ریمو) (chat)$",
+  "^(ریمو) (realm)$",
+  "^(تنزل) (.*)$",
+  "^(تنزل)",
+  "^(تنظیم) ([^%s]+) (.*)$",
+  "^(قفل) (.*)$",
+  "^(صاحب) (%d+)$",
+  "^(صاحب)",
+  "^(دارنده)$",
+  "^(ایدی) (.*)$",
   "^[#!/](setgpowner) (%d+) (%d+)$",-- (group id) (owner id)
-  "^[#!/](unlock) (.*)$",
-  "^[#!/](setflood) (%d+)$",
-  "^[#!/](settings)$",
-  "^[#!/](public) (.*)$",
-  "^[#!/](modlist)$",
-  "^[#!/](newlink)$",
-  "^[#!/](link)$",
-  "^[#!/]([Mm]ute) ([^%s]+)$",
-  "^[#!/]([Uu]nmute) ([^%s]+)$",
-  "^[#!/]([Mm]uteuser)$",
-  "^[#!/]([Mm]uteuser) (.*)$",
-  "^[#!/]([Mm]uteslist)$",
-  "^[#!/]([Mm]utelist)$",
-  "^[#!/](kickinactive)$",
-  "^[#!/](kickinactive) (%d+)$",
+  "^(بازکردن) (.*)$",
+  "^(حساسیت) (%d+)$",
+  "^(تنظیمات)$",
+  "^(عمومی) (.*)$",
+  "^(مدیران)$",
+  "^(لینک جدید)$",
+  "^(لینک)$",
+  "^(سکوت) ([^%s]+)$",
+  "^(آزاد) ([^%s]+)$",
+  "^(سکوت)$",
+  "^(سکوت) (.*)$",
+  "^(لیست سکوت)$",
+  "^(لیست سکوت)$",
+  "^(حذف غیرفعال)$",
+  "^(حذف غیرفعال) (%d+)$",
   "%[(document)%]",
   "%[(photo)%]",
   "%[(video)%]",
